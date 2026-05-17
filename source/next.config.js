62ea4b41fe65b7258d5ad0a77e4970ef27c90ca9
cosmoart <cosmohydra17@gmail.com>
2026-05-17 11:46:41 -0500

    🛠️ Update dependencies

--- source/next.config.js
+++ source/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['picsum.photos', 'multimedia.infojobs.net', 'www.spain.info', 'hips.hearstapps.com', 'a1.eestatic.com']
+		remotePatterns: [
+			{ protocol: 'https', hostname: 'picsum.photos' },
+			{ protocol: 'https', hostname: 'multimedia.infojobs.net' },
+			{ protocol: 'https', hostname: 'www.spain.info' },
+			{ protocol: 'https', hostname: 'hips.hearstapps.com' },
+			{ protocol: 'https', hostname: 'a1.eestatic.com' }
+		]
 	}
 }
 
